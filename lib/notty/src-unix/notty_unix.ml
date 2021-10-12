0e8c9d5d2699ab1090af74d1397faa7bd5bc1f09
Etienne Millon <me@emillon.org>
2021-10-12 13:34:33 +0200

    Add 4.13 lockfile
    Closes #5

--- lib/notty/src-unix/notty_unix.ml
+++ lib/notty/src-unix/notty_unix.ml
@@ -1,6 +1,8 @@
 (* Copyright (c) 2016-2017 David Kaloper Meršinjak. All rights reserved.
    See LICENSE.md. *)
 
+[@@@ocaml.warning "-5"]
+
 open Notty
 
 external c_winsize : Unix.file_descr -> int = "caml_notty_winsize" [@@noalloc]
